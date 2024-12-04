2c90a09649214f073f0195450f00b4d6b89bb03e
meoww4 <mashafetrovav@gmail.com>
2024-12-04 16:10:31 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Main from "./pages/main";
+
+import Login from "./pages/login";
+import Search from "./pages/search";
+import Registration from "./pages/registration";
+import Add_Pet from "./pages/add_pet";
+import {Routes, Route} from "react-router-dom";
+import Profile from "./pages/profile";
+
+import '../src/components/styles/login.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path={"/"} element={<Main />} />
+          <Route path={"/profile"} element={<Profile/>} />
+          <Route path={"/login"} element={<Login />} />
+          <Route path={"/search"} element={<Search />} />
+          <Route path={"/registration"} element={<Registration />} />
+          <Route path={"/add_pet"} element={<Add_Pet />} />
+        </Routes>
+        <Footer />
+      </div>
+    
   );
 }
 
